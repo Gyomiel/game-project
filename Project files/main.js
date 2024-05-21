a3f1c13799cfd1c09dd389f54ae743500600e144
Gyomiel <rosadrdlm@gmail.com>
2024-05-21 11:32:51 +0100

    GENERAL: 1.Start Screen 1.Background image 1.2 audio 1.3 Start button

--- Project files/main.js
+++ Project files/main.js
@@ -1,12 +1,28 @@
 var canvas = document.getElementById("screen");
+var startButton = document.getElementsByClassName("btn")[0];
+var startScreen =  document.getElementById("startScreen");
 
 // Character
 
-let character = new Character(400, 500);
+let character = new Character(475, 500);
 
 // Starting the game
 
 function startGame() {
     character.insertCharacter();
     
+    
+
 }
+
+startButton.addEventListener("click", () => {
+    
+    
+    canvas.style.display = "block";
+    startScreen.style.display = "none";
+    startButton.style.display = "none";
+    
+    startGame();
+})
+
+
---
+++ Project files/startscreen.js
@@ -0,0 +1,5 @@
+/*let startScreen = document.createElement("img");
+startScreen.setAttribute("src", "../images/StartScreen.png");
+
+window.onload =function () {
+    document.getElementById("startScreen").appendChild(startScreen);*/
