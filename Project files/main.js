eed6830f98f3debae22b07fe04d0ee74fbfc77b8
Gyomiel <rosadrdlm@gmail.com>
2024-05-22 16:39:38 +0100

    GENERAL 1.Check collisions 2. Improve movement with "speed variable"

--- Project files/main.js
+++ Project files/main.js
@@ -2,95 +2,90 @@
 
 let canvas = document.getElementById("screen"); // Makes the id="screen" into a variable named canvas.
 canvas.style.width = 1000 + "px";
-canvas.style.height = 1000 +"px";
+canvas.style.height = 1000 + "px";
 let startButton = document.getElementsByClassName("btn")[0]; // Button that starts the game from the title screen.
-let startScreen =  document.getElementById("startScreen"); // Makes the id="startScreen" into a variable named startScreen.
+let startScreen = document.getElementById("startScreen"); // Makes the id="startScreen" into a variable named startScreen.
 let heyListen = document.getElementById("heyListen"); // "Hey, listen!" sound effect for the button.
+let stOST = document.getElementById("stOST"); // "The Hero of Rhyme" soundtrack for the title screen.
 let timerMoveCharacterX; // Moving time interval on X axis.
 let timerMoveCharacterY; // Moving time interval on Y axis.
-let timerMoveEnemyX;
-let timerMoveEnemyY;
+let timerCreateEnemy
 
+//creamos array de enemigos
+let enemyArray = [];
+
+function createEnemy() {
+    if (enemyArray.length <= 2) {
+        
+   
+    console.log("listen")
+    let randomLocationY = Math.floor(Math.random() * (heightInt - 800 + 1) + 800 - 40);
+
+    let randomLocationX = Math.floor(Math.random() * (widthInt) - 40)
+
+    let enemy = new Enemy(randomLocationX, randomLocationY);
+
+    enemy.insertEnemy();
+    enemyArray.push(enemy); }
+}
 // Character
 
-let character = new Character(475, 500); // Sets the character into the canvas.
-
-// Enemies
 
 let widthInt = canvas.style.width.slice(0, 4);
 let heightInt = canvas.style.width.slice(0, 4);
-
-let randomLocationX = Math.floor(Math.random() * (widthInt) - 40)
-console.log(randomLocationX);
-
-let randomLocationY = Math.floor(Math.random() * (heightInt - 800 + 1) + 800 - 40);
-let enemy1 = new Enemy(randomLocationX, randomLocationY); // Sets the enemy into the canvas.
-let randomLocationX2 = Math.floor(Math.random() * (widthInt) - 40)
-let randomLocationY2 = Math.floor(Math.random() * (heightInt - 200 + 1) + 200 - 40);
-let enemy2 = new Enemy(randomLocationX2, randomLocationY2);
-let randomLocationX3 = Math.floor(Math.random() * (widthInt) - 40)
-let randomLocationY3 = Math.floor(Math.random() * (heightInt - 400  + 1) + 400 - 40);
-let enemy3 = new Enemy(randomLocationX3, randomLocationY3);
+let character = new Character(475, 500); // Sets the character into the canvas.
 
 
 // Starting the game
 
-let stOST = document.getElementById("stOST"); // "The Hero of Rhyme" soundtrack for the title screen.
 
 function startGame() { // Starts the game.
     character.insertCharacter(); // Inserts the character into the game.
-    enemy1.insertEnemy(); // Inserts enemies into the game.
-    enemy2.insertEnemy();
-    enemy3.insertEnemy();
     
-    
-    timerMoveCharacterX = setInterval(() => 
-        character.characterMovementX() , 20);
 
-    timerMoveCharacterY = setInterval(() => 
-        character.characterMovementY() , 20);
 
-    timerMoveEnemyX = setInterval(() =>
-        enemy1.enemyMovementX(), 250);
+    timerMoveCharacterX = setInterval(() =>
+        character.characterMovementX(), 20);
 
-    timerMoveEnemyX = setInterval(() =>
-        enemy2.enemyMovementX(), 250);
+    timerMoveCharacterY = setInterval(() =>
+        character.characterMovementY(), 20);
 
-    timerMoveEnemyX = setInterval(() =>
-        enemy3.enemyMovementX(), 250);
-    
-    timerMoveEnemyY = setInterval(() =>
-        enemy1.enemyMovementY(), 250);
 
-    timerMoveEnemyY = setInterval(() =>
-        enemy2.enemyMovementY(), 250);
+    timerCreateEnemy = setInterval(() =>
+        createEnemy(), 1000)
 
-    timerMoveEnemyY = setInterval(() =>
-        enemy3.enemyMovementY(), 250);
 
 }
 
 
 // Title screen "Start" button
 
+startScreen.addEventListener("click", () => {
+    let bgST = setInterval(() => {
+        stOST.play();
+    }, 10);
+
+})
+
 startButton.addEventListener("click", () => {
-    
+
+
     heyListen.play(); // Plays the "Hey, Listen!" sound effect when the button is clicked.
-    
+
     setTimeout(() => {
         canvas.style.display = "block"; // Hides the canvas while the title screen is on.
-    startScreen.style.display = "none"; // Shows the title screen.
-    startButton.style.display = "none"; // Shows the "Start" button.
+        startScreen.style.display = "none"; // Shows the title screen.
+        startButton.style.display = "none"; // Shows the "Start" button.
     }, 2000);
-    
+
     startGame(); // Starts the game.
 });
 
 
 // Keybinding 
 
-window.addEventListener("keydown", function(e) {
-    switch(e.key) {
+window.addEventListener("keydown", function (e) {
+    switch (e.key) {
         case "a":
             character.directionX = -1;
             break;
@@ -106,8 +101,8 @@ window.addEventListener("keydown", function(e) {
     }
 })
 
-window.addEventListener("keyup", function(e) {
-    switch(e.key) {
+window.addEventListener("keyup", function (e) {
+    switch (e.key) {
         case "a":
         case "d":
             character.directionX = 0;
--- Project files/worldenemies.js
+++ Project files/worldenemies.js
@@ -7,12 +7,13 @@ class Enemy { // Creates the enemy.
         this.width = 40;
         this.height = 40;
         this.sprite = document.createElement("div");
-        this.directionX = null;
-        this.directionY = null;
-        this.speed = 35;
-        
+        this.speed = 1;
+        this.timerMove = setInterval(() => this.enemyMovement(), 10)
+        this.directionRandom = 0
+        this.timerDirection = setInterval(() => this.randomNumber(), 1000)
+
     }
-    
+
     insertEnemy() { // Inserts the enemy into the canvas.
         console.log(this.y, this.x)
         this.sprite.setAttribute("id", "enemy");
@@ -21,67 +22,93 @@ class Enemy { // Creates the enemy.
         this.sprite.style.width = this.width + "px";
         this.sprite.style.height = this.height + "px";
         canvas.appendChild(this.sprite);
-        
+
     }
-    
-    enemyMovementX() { // Moves the character in its X axis (left and right).
-        let enemyDirectionArrayX = [0, 1, -1];
-        let enemyRandomDirectionX = enemyDirectionArrayX[Math.floor(Math.random() * 3)];
-        let enemyMoveX = this.x + this.speed * enemyRandomDirectionX;
-        
-        if (enemyMoveX <= 1000 - this.width && enemyMoveX >= 0) {
-            this.x = enemyMoveX;
-            this.sprite.style.left = this.x + "px";
+
+    randomNumber() {
+        this.directionRandom = Math.floor(Math.random() * 4) + 1;
+    }
+
+    enemyMovement() { // Moves the character in its X axis (left and right).
+
+
+        console.log(this.directionRandom)
+
+        if (this.directionRandom === 1) {
+            let enemyMoveX = this.x + this.speed * 1;
+            if (enemyMoveX <= 1000 - this.width && enemyMoveX >= 0) {
+                this.x = enemyMoveX;
+                this.checkCollisions();
+                this.sprite.style.left = this.x + "px";
+            }}
+
+            if (this.directionRandom === 2) {
+                let enemyMoveX = this.x + this.speed * -1;
+                if (enemyMoveX <= 1000 - this.width && enemyMoveX >= 0) {
+                    this.x = enemyMoveX;
+                    this.checkCollisions();
+                    this.sprite.style.left = this.x + "px";
+                }}
+                if (this.directionRandom === 3) {
+                    let enemyMoveY = this.y + this.speed * +1;
+                    if (enemyMoveY <= 1000 - this.height && enemyMoveY >= 0) {
+                        this.y = enemyMoveY;
+                        this.checkCollisions();
+                        this.sprite.style.top = this.y + "px";
+                    }
+                }
+                if (this.directionRandom === 4) {
+                    let enemyMoveY = this.y + this.speed * -1;
+                    if (enemyMoveY <= 1000 - this.height && enemyMoveY >= 0) {
+                        this.y = enemyMoveY;
+                        this.checkCollisions();
+                        this.sprite.style.top = this.y + "px";
+                    }
+                }
+
+            }
+
+            removeEnemy() {
+                canvas.removeChild(this.sprite);
+                clearInterval(timerMoveEnemyX)
+                clearInterval(timerMoveEnemyY)
+
+
+            }
+
+            checkCollisions() {
+                let eVertexTLX = this.x;
+                let eVertexTLY = this.y;
+                let eVertexTRX = this.x + this.width;
+                let eVertexTRY = this.y;
+
+                let eVertexBLX = this.x;
+                let eVertexBLY = this.y + this.height;
+                let eVertexBRX = this.x + this.width;
+                let eVertexBRY = this.y + this.height;
+
+
+                let charVertexTLX = character.x;
+                let charVertexTLY = character.y;
+                let charVertexTRX = character.x + character.width;
+                let charVertexTRY = character.y;
+
+                let charVertexBLX = character.x;
+                let charVertexBLY = character.y + character.height;
+                let charVertexBRX = character.x + character.width;
+                let charVertexBRY = character.y + character.height;
+
+
+                if (eVertexBLX >= charVertexTLX && eVertexBLX <= charVertexTRX && eVertexTRY >= charVertexTLY && eVertexTRY <= eVertexBLY || eVertexBRX >= charVertexTLX && eVertexBRX <= charVertexTRX && eVertexBLY >= charVertexTRY && eVertexBRY <= charVertexBRY) {
+                    this.removeEnemy();
+                    console.log("Auch")
+                }
+            }
+
+
+
+
         }
-    }
-
-    enemyMovementY() { // Moves the character in its Y axis (up and down).
-        let enemyDirectionArrayY = [0, 1, -1];
-        let enemyRandomDirectionY = enemyDirectionArrayY[Math.floor(Math.random() * 3)];        
-        let enemyMoveY = this.y + this.speed * enemyRandomDirectionY;
-      
-        if (enemyMoveY <= 1000 - this.height && enemyMoveY >= 0) {
-            this.y = enemyMoveY;
-            this.sprite.style.top = this.y + "px";
-        }
-    }
-
-    removeEnemy() {
-        canvas.removeChild(this.sprite);
-    }
-
-    checkCollisions() {
-        let eVertexTLX = this.x;
-        let eVertexTLY = this.y;
-        let eVertexTRX = this.x + this.width;
-        let eVertexTRY = this.y;
-
-        let eVertexBLX = this.x;
-        let eVertexBLY = this.y + this.height;
-        let eVertexBRX = this.x + this.width;
-        let eVertexBRY = this.y + this.height;
-        
-
-        let charVertexTLX = character.x;
-        let charVertexTLY = character.y;
-        let charVertexTRX = character.x + character.width;
-        let charVertexTRY = character.y;
-
-        let charVertexBLX = character.x;
-        let charVertexBLY = character.y + character.height;
-        let charVertexBRX = character.x + character.width;
-        let charVertexBRY = character.y + character.height;
-
-
-        if (eVertexBLX >= charVertexTLX && eVertexBLX <= charVertexTRX || eVertexBRX >= charVertexTLX && eVertexBRX <= charVertexTRX) {
-            this.removeEnemy();
-        }
-    }
-
-
-
-
-}
 
 
 
